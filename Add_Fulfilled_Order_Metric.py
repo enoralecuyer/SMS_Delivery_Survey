f16d6f885fe705fdb89a7c9816d191aa02d5cd89
enoralecuyer <48727972+enoralecuyer@users.noreply.github.com>
2022-09-20 20:01:14 -0700

    Update Add_Fulfilled_Order_Metric.py

--- Add_Fulfilled_Order_Metric.py
+++ Add_Fulfilled_Order_Metric.py
@@ -7,5 +7,10 @@ def sendToKlaviyo():
   client.Public.track(
     'Fulfilled Order',
     email='test@gmail.com',
+#     customer_properties={
+#       "$first_name":"FIRST_NAME",
+#       "phone_number":"PHONE_NUMBER",
+#       "$city":"CITY, STATE",
+#     }, 
   )
 sendToKlaviyo()
